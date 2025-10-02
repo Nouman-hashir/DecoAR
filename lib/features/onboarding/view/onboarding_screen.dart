30068e423678a8da0fea9132c6fc7eebf2423e21
Code_with_nouman <134682051+Nouman-hashir@users.noreply.github.com>
2025-10-02 17:13:57 +0500

    Add custom button widgets and update onboarding UI
    Introduced a reusable CustomAppButtons widget library for various button styles. Updated onboarding_screen to use these custom buttons and improved its layout and visuals. Also exported custom_button.dart in utils.dart and initialized ScreenUtil in main.dart for responsive design.

--- lib/features/onboarding/view/onboarding_screen.dart
+++ lib/features/onboarding/view/onboarding_screen.dart
@@ -1,12 +1,60 @@
-import 'package:flutter/material.dart';
+import '../../../decoar.dart';
 
-class OnboardingScreen extends StatelessWidget {
+class OnboardingScreen extends StatefulWidget {
   const OnboardingScreen({super.key});
 
   @override
+  State<OnboardingScreen> createState() => _OnboardingScreenState();
+}
+
+class _OnboardingScreenState extends State<OnboardingScreen> {
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(children: [Image.asset('assets/images/bg.png')]),
+      body: Stack(
+        children: [
+          Positioned.fill(
+            child: Image.asset('assets/images/bg.png', fit: BoxFit.cover),
+          ),
+          Column(
+            children: [
+              Expanded(
+                child: Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Image.asset(
+                        'assets/images/icon.png',
+                        width: 100,
+                        height: 100,
+                      ),
+                      SizedBox(height: 80.h),
+                      Text(
+                        "Discover your Dream \n      furniture here",
+                        style: AppTextStyles.poppins(
+                          color: AppColors.green,
+                          fontSize: 26.sp,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(bottom: 50.h),
+                child: CustomAppButtons.footerBtn(
+                  label: "Get Started",
+                  onTap: () {},
+                  color: AppColors.black,
+                  textColor: AppColors.white,
+                  width: 170.w,
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
